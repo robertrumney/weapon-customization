eea3adaf4c8c798aa06a05f6d1aaed17669196ad
Robert Rumney <robertrumney@gmail.com>
2023-04-20 23:45:36 +0200

    Update CustomWeapon.cs

--- CustomWeapon.cs
+++ CustomWeapon.cs
@@ -1,31 +1,41 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class CustomWeapon : MonoBehaviour
 {
+    [Header("Base Gun")]
     public GameObject baseGun;
+
+    [Header("Modules")]
     public List<GameObject> magazineTypes;
     public List<GameObject> barrelTypes;
     public List<GameObject> scopeTypes;
 
-    public WeaponBehavior weaponBehaviour;
-    public AudioClip[] barrelAudioClips;
-    public GameObject crosshairUI;
-
+    [Header("Current Modules")]
     public int currentMagazine;
     public int currentBarrel;
     public int currentScope;
 
+    [Header("Module Unlocked States")]
     public bool[] isMagazineUnlocked;
     public bool[] isBarrelUnlocked;
     public bool[] isScopeUnlocked;
 
+    [Header("Weapon Behavior")]
+    public WeaponBehavior weaponBehaviour;
+
+    [Header("Barrel Audio Clips")]
+    public AudioClip[] barrelAudioClips;
+
+    [Header("UI Components")]
+    public GameObject crosshairUI;
+
+    [Header("Private Components")]
     private AudioSource audioSource;
 
     [SerializeField]
-    private FlameThrowerBehaviour flamethrowerScript;
+    private GameObject flamethrowerScript;
 
     private void Start()
     {
@@ -39,6 +49,7 @@ public class CustomWeapon : MonoBehaviour
         currentMagazine = 0;
         currentBarrel = 0;
         currentScope = 0;
+
         SwitchMagazine(currentMagazine);
         SwitchBarrel(currentBarrel);
         SwitchScope(currentScope);
@@ -48,11 +59,14 @@ public class CustomWeapon : MonoBehaviour
     {
         if (isMagazineUnlocked[index])
         {
-            magazineTypes[currentMagazine].SetActive(false);
-            magazineTypes[index].SetActive(true);
+            if (magazineTypes[currentMagazine])
+                magazineTypes[currentMagazine].SetActive(false);
+
+            if (magazineTypes[index])
+                magazineTypes[index].SetActive(true);
 
             // Example of adjusting ammo based on magazine type
-            weaponBehaviour.bulletsPerClip = index * 10; 
+            weaponBehaviour.bulletsPerClip = index * 10;
 
             currentMagazine = index;
         }
@@ -62,8 +76,12 @@ public class CustomWeapon : MonoBehaviour
     {
         if (isBarrelUnlocked[index])
         {
-            barrelTypes[currentBarrel].SetActive(false);
-            barrelTypes[index].SetActive(true);
+            if (barrelTypes[currentBarrel])
+                barrelTypes[currentBarrel].SetActive(false);
+
+            if (barrelTypes[index])
+                barrelTypes[index].SetActive(true);
+
             audioSource.clip = barrelAudioClips[index];
             currentBarrel = index;
         }
@@ -73,8 +91,11 @@ public class CustomWeapon : MonoBehaviour
     {
         if (isScopeUnlocked[index])
         {
-            scopeTypes[currentScope].SetActive(false);
-            scopeTypes[index].SetActive(true);
+            if (scopeTypes[currentScope])
+                scopeTypes[currentScope].SetActive(false);
+
+            if (scopeTypes[index])
+                scopeTypes[index].SetActive(true);
 
             // Check for "no scope" and enable UI crosshair
             if (index == 0)
@@ -89,11 +110,11 @@ public class CustomWeapon : MonoBehaviour
             // Check for flamethrower scope and enable flamethrower script
             if (index == 2)
             {
-                flamethrowerScript.enabled = true;
+                flamethrowerScript.SetActive(true);
             }
             else
             {
-                flamethrowerScript.enabled = false;
+                flamethrowerScript.SetActive(false);
             }
 
             currentScope = index;
@@ -101,7 +122,7 @@ public class CustomWeapon : MonoBehaviour
     }
 
     // Example preset combination
-    public void ApplyPreset1()
+    public void ApplyPreset()
     {
         SwitchMagazine(0);
         SwitchBarrel(1);
